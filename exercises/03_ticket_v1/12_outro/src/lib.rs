2b5b93cbb191c8f274105eef970b45b556317f40
Woojun Ro <woojunro0223@gmail.com>
2025-01-21 11:20:04 -0800

    03_ticket_v1/12_outro

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,74 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+pub struct Order {
+  product_name: String,
+  quantity: u64,
+  unit_price: u64
+}
+
+impl Order {
+  pub fn new(product_name: String, quantity: u64, unit_price: u64) -> Order {
+    validate_product_name(&product_name);
+    validate_quantity(&quantity);
+    validate_unit_price(&unit_price);
+
+    Order {
+      product_name,
+      quantity,
+      unit_price
+    }
+  }
+
+  pub fn product_name(&self) -> &String {
+    &self.product_name
+  }
+
+  pub fn quantity(&self) -> &u64 {
+    &self.quantity
+  }
+
+  pub fn unit_price(&self) -> &u64 {
+    &self.unit_price
+  }
+
+  pub fn set_product_name(&mut self, product_name: String) {
+    validate_product_name(&product_name);
+    self.product_name = product_name;
+  }
+
+  pub fn set_quantity(&mut self, quantity: u64) {
+    validate_quantity(&quantity);
+    self.quantity = quantity;
+  }
+
+  pub fn set_unit_price(&mut self, unit_price: u64) {
+    validate_unit_price(&unit_price);
+    self.unit_price = unit_price;
+  }
+
+  pub fn total(&self) -> u64 {
+    self.quantity * self.unit_price
+  }
+}
+
+fn validate_product_name(product_name: &String) {
+  if product_name.is_empty() {
+    panic!("The product name can't be empty.");
+  }
+  if product_name.len() > 300 {
+    panic!("The product name can't be longer than 300 bytes.");
+  }
+}
+
+fn validate_quantity(quantity: &u64) {
+  if *quantity <= 0 {
+    panic!("The quantity must be strictly greater than zero.");
+  }
+}
+
+fn validate_unit_price(unit_price: &u64) {
+  if *unit_price <= 0 {
+    panic!("The unit price must be strictly greater than zero.");
+  }
+}
